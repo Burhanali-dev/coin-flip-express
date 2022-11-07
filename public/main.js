4904de84871884f61ff794bb5988ebb049cf3819
Burhan Ali <burhanali.dev@gmail.com>
2022-11-07 01:50:17 -0500

    update

--- public/main.js
+++ public/main.js
@@ -1,34 +1,28 @@
-document.getElementById("headsBtn").addEventListener('click', function(){
-  console.log("heads button clicked")
-  fetch(`/api?answer=${"heads"}`)
-      .then(response => response.json())
-      .then((data) => {
-        console.log(data);
-        if (data.check == true) {
-        document.getElementById("result").innerHTML = "You guessed correctly!";
-        document.getElementById("coin").src='https://i.ebayimg.com/images/g/xtcAAOSwLwBaZigS/s-l400.jpg';
-        }
-        else { 
-        document.getElementById("result").innerHTML = "You guessed incorrectly!";
-        document.getElementById("coin").src="https://m.media-amazon.com/images/I/51bcZy+HZpL._AC_SY580_.jpg";
-        }
-      });
-     
-  })
-  document.getElementById("tailsBtn").addEventListener('click', function(){
-    console.log("tails button clicked")
-    fetch(`/api?answer=${"tails"}`)
-        .then(response => response.json())
-        .then((data) => {
-          console.log(data);
-          if (data.check == true){ 
-          document.getElementById("result").innerHTML = "You guessed correctly!";
-          document.getElementById("coin").src="https://m.media-amazon.com/images/I/51bcZy+HZpL._AC_SY580_.jpg"
-          }
-          else { 
-          document.getElementById("result").innerHTML = "You guessed incorrectly!";
-          document.getElementById("coin").src='https://i.ebayimg.com/images/g/xtcAAOSwLwBaZigS/s-l400.jpg';
-          }
-        });
-  
+
+
+function createE(value) {
+    fetch('create', {
+      method: 'put',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        'result': value
+      })
+    }).then(response => {
+      if (response.ok) console.log('coinflip updated')
+      window.location.reload()
+      
     })
+}
+function deleteE() {
+  fetch('delete', {
+    method: 'delete',
+    headers: {'Content-Type': 'application/json'},
+  }).then(response => {
+    if (response.ok) console.log('coinflip updated')
+    window.location.reload()
+    
+  })
+}
+
+
+
--- server.js
+++ server.js
@@ -5,57 +5,47 @@ const MongoClient = require('mongodb').MongoClient
 
 var db, collection;
 
-const url = "mongodb+srv://demo:demo@cluster0-q2ojb.mongodb.net/test?retryWrites=true";
-const dbName = "demo";
+const url = "mongodb+srv://burhanali:2short2me@cluster0.e7moc1a.mongodb.net/coin-flip?retryWrites=true&w=majority";
+const dbName = "coin-flip";
 
 app.listen(3000, () => {
-    MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
-        if(error) {
-            throw error;
-        }
-        db = client.db(dbName);
-        console.log("Connected to `" + dbName + "`!");
-    });
+  MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+    if (error) {
+      throw error;
+    }
+    db = client.db(dbName);
+    console.log("Connected to `" + dbName + "`!");
+  });
 });
 
 app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  db.collection('messages').find().toArray((err, result) => {
+  db.collection('coinflip').find().toArray((err, result) => {
     if (err) return console.log(err)
-    res.render('index.ejs', {messages: result})
+    res.render('index.ejs', { coinflip: result })
   })
 })
 
-app.post('/messages', (req, res) => {
-  db.collection('messages').insertOne({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
-    if (err) return console.log(err)
-    console.log('saved to database')
-    res.redirect('/')
-  })
+app.put('/create', (req, res) => {
+  const collection = db.collection('coinflip')
+  let result = Math.random() > 0.5 ? 0 : 1;
+  collection.insertMany([{
+    result: req.body.result == result ? "You Win" : "You Lose"
+  }])
+  console.log('saved to database')
+  res.send('coinflip created!')
 })
 
-app.put('/messages', (req, res) => {
-  db.collection('messages')
-  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
-    $set: {
-      thumbUp:req.body.thumbUp + 1
-    }
-  }, {
-    sort: {_id: -1},
-    upsert: true
-  }, (err, result) => {
-    if (err) return res.send(err)
-    res.send(result)
-  })
-})
 
-app.delete('/messages', (req, res) => {
-  db.collection('messages').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
+
+app.delete('/delete', (req, res) => {
+  db.collection('coinflip').deleteMany({}, 
+    (err, result) => {
     if (err) return res.send(500, err)
-    res.send('Message deleted!')
+    res.send('coinflip deleted!')
   })
 })
